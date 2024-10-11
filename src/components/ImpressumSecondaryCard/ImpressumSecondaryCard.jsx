e0eb19ef01e47cbd740397567353096baf9d4f43
Ehtesham-Zahid <shamimalick321@gmail.com>
2024-10-11 12:20:36 +0500

    Datenschutz Page Started

--- src/components/ImpressumSecondaryCard/ImpressumSecondaryCard.jsx
+++ src/components/ImpressumSecondaryCard/ImpressumSecondaryCard.jsx
@@ -2,9 +2,15 @@ const ImpressumSecondaryCard = (props) => {
   return (
     <div>
       <p className="text-black  text-4xl font-[550] ">{props.title}</p>
-      <p className=" mt-16 text-xl leading-relaxed font-thin">{props.text1}</p>
-      <p className="text-xl leading-relaxed font-thin">{props.text2}</p>
-      <p className="text-xl leading-relaxed font-thin">{props.text3}</p>
+      <p className=" mt-16 text-base sm:text-lg md:text-xl leading-relaxed font-thin">
+        {props.text1}
+      </p>
+      <p className="text-base sm:text-lg md:text-xl leading-relaxed font-thin">
+        {props.text2}
+      </p>
+      <p className="text-base sm:text-lg md:text-xl leading-relaxed font-thin">
+        {props.text3}
+      </p>
     </div>
   );
 };
