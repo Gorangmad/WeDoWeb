66bbbef4d7cb0171bb27c14d4142b64358221726
Ehtesham-Zahid <shamimalick321@gmail.com>
2024-10-16 01:01:26 +0500

    Datenschutz Page Started

--- src/components/ImpressumSecondaryCard/ImpressumSecondaryCard.jsx
+++ src/components/ImpressumSecondaryCard/ImpressumSecondaryCard.jsx
@@ -1,14 +1,16 @@
 const ImpressumSecondaryCard = (props) => {
   return (
     <div>
-      <p className="text-black  text-4xl font-[550] ">{props.title}</p>
-      <p className=" mt-16 text-base sm:text-lg md:text-xl leading-relaxed ">
+      <p className="text-black  text-3xl lg:text-4xl font-[550] ">
+        {props.title}
+      </p>
+      <p className=" mt-16 text-base sm:text-lg lg:text-xl leading-relaxed ">
         {props.text1}
       </p>
-      <p className="text-base sm:text-lg md:text-xl leading-relaxed ">
+      <p className="text-base sm:text-lg lg:text-xl leading-relaxed ">
         {props.text2}
       </p>
-      <p className="text-base sm:text-lg md:text-xl leading-relaxed ">
+      <p className="text-base sm:text-lg lg:text-xl leading-relaxed ">
         {props.text3}
       </p>
     </div>
--- src/components/ImpressumSection/ImpressumSection.jsx
+++ src/components/ImpressumSection/ImpressumSection.jsx
@@ -4,15 +4,23 @@ import ImpressumSecondaryCard from "../ImpressumSecondaryCard/ImpressumSecondary
 const ImpressumSection = () => {
   return (
     <section className="w-11/12 min-[450px]:w-5/6 mx-auto flex flex-col">
-      <p className="text-black  text-5xl font-semibold mt-20">Impressum</p>
+      <p className="text-black   text-4xl lg:text-5xl font-semibold mt-20 flex items-center flex-col sm:flex-row">
+        Impressum
+        <span className="text-lg mt-3 ms-2  "> (Angaben gemäß § 5 TMG)</span>
+      </p>
       <div className="flex flex-col items-start gap-y-12 mt-16">
         <ImpressumCard
           title="WEDO"
-          text1="Wenske, Sebastian & Cetin, Baris GbR"
-          text2="Ohlauer Straße 2"
-          text3="10999 Berlin"
+          text1="Gorang Madaan"
+          text2="Kurmainzerstr. 64"
+          text3="65929 Frankfurt am Main"
         />
         <ImpressumCard
+          title="Vertreten durch"
+          text1="Gorang Madaan"
+          text2="Hazem Dawood"
+        />
+        {/* <ImpressumCard
           title="Kontakt"
           text1="Tel: 030 - 469 990 798"
           text2="E-Mail: info@space-rocket.de"
@@ -35,37 +43,38 @@ const ImpressumSection = () => {
           text2="IBAN: DE96 1004 0000 0672 4405 00"
           text3="BIC: COBADEFFXXX"
           text4="USt-ID: DE322172389"
-        />
+        /> */}
       </div>
       <div className="flex flex-col items-start gap-y-12 my-16">
+        <p className="text-black  text-2xl md:text-3xl lg:text-4xl font-[550] text-center uppercase w-full ">
+          Haftungsausschluss
+        </p>
+        <ImpressumSecondaryCard
+          title="Haftung für Inhalte"
+          text1="Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+"
+        />
+        <ImpressumSecondaryCard
+          title="Haftung für Links"
+          text1="Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen."
+        />
         <ImpressumSecondaryCard
           title="Urheberrecht"
-          text1="Falls nicht anders angegeben, unterliegen alle Seiten dieses
-        Webangebotes dem Urheberrecht (Copyright). Dies gilt insbesondere für
-        Texte, Bilder, Grafiken und Style-Dateien, einschließlich deren
-        Anordnung auf den Websites."
-          text2="Eine Vervielfältigung oder Verwendung von Websites (oder von
-        Teilen daraus) in anderen elektronischen oder gedruckten Publikationen
-        und deren Veröffentlichung (auch im Internet) ist nur nach vorheriger
-        Genehmigung gestattet."
-          text3="Weiterhin können Bilder, Grafiken, Text- oder sonstige Dateien
-        ganz oder teilweise dem Urheberrecht Dritter unterliegen. Alle innerhalb
-        des Internetangebotes genannten und ggf. durch Dritte geschützten
-        Marken- und Warenzeichen unterliegen uneingeschränkt den Bestimmungen
-        des jeweils gültigen Kennzeichenrechts und den Besitzrechten der
-        jeweiligen eingetragenen Eigentümer. Allein aufgrund der bloßen Nennung
-        in unserem Internetangebot ist nicht der Schluss zu ziehen, dass
-        Markenzeichen nicht durch Rechte Dritter geschützt sind."
+          text1="Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen."
         />
         <ImpressumSecondaryCard
-          title="Haftungsausschluss"
-          text1="Der Autor hat alle in seinem Bereich bereitgestellten Informationen nach bestem Wissen und Gewissen erarbeitet und geprüft. Jedoch übernimmt der Autor keinerlei Gewähr für die Aktualität, Korrektheit, Vollständigkeit oder Qualität der bereitgestellten Informationen. Haftungsansprüche gegen den Autor, welche sich auf Schäden materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen verursacht wurden, sind grundsätzlich ausgeschlossen, sofern seitens des Autors kein nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt."
-          text2="Alle Angebote sind freibleibend und unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen."
+          title="Datenschutz"
+          text1="Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben."
+          text2="Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich."
+          text3="Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor."
         />
         <ImpressumSecondaryCard
-          title="Verweise und Links (Disclaimer)"
-          text1="Bei direkten oder indirekten Verweisen auf fremde Websites ('Hyperlinks'), die außerhalb des Verantwortungsbereiches des Autors liegen, würde eine Haftungsverpflichtung ausschließlich in dem Fall in Kraft treten, in dem der Autor von den Inhalten Kenntnis hat und es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern."
-          text2="Der Autor erklärt hiermit ausdrücklich, dass zum Zeitpunkt der Linksetzung keine illegalen Inhalte auf den zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige Gestaltung, die Inhalte oder die Urheberschaft der gelinkten/verknüpften Seiten hat der Autor keinerlei Einfluss. Deshalb distanziert er sich hiermit ausdrücklich von allen Inhalten aller gelinkten/verknüpften Seiten, die nach der Linksetzung verändert wurden. Diese Feststellung gilt für alle innerhalb des eigenen Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Autor eingerichteten Gästebüchern, Diskussionsforen und Mailinglisten. Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist."
+          title="Google Analytics"
+          text1="Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. (''Google''). Google Analytics verwendet sog. ''Cookies'', Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglicht. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website (einschließlich Ihrer IP-Adresse) wird an einen Server von Google in den USA übertragen und dort gespeichert. Google wird diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten für die Websitebetreiber zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte übertragen, sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten der Google in Verbindung bringen. Sie können die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browser Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website voll umfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden."
+        />
+        <ImpressumSecondaryCard
+          title="Google AdSense"
+          text1="Diese Website benutzt Google Adsense, einen Webanzeigendienst der Google Inc., USA (''Google''). Google Adsense verwendet sog. ''Cookies'' (Textdateien), die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglicht. Google Adsense verwendet auch sog. ''Web Beacons'' (kleine unsichtbare Grafiken) zur Sammlung von Informationen. Durch die Verwendung des Web Beacons können einfache Aktionen wie der Besucherverkehr auf der Webseite aufgezeichnet und gesammelt werden. Die durch den Cookie und/oder Web Beacon erzeugten Informationen über Ihre Benutzung dieser Website (einschließlich Ihrer IP-Adresse) werden an einen Server von Google in den USA übertragen und dort gespeichert. Google wird diese Informationen benutzen, um Ihre Nutzung der Website im Hinblick auf die Anzeigen auszuwerten, um Reports über die Websiteaktivitäten und Anzeigen für die Websitebetreiber zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte übertragen, sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten der Google in Verbindung bringen. Das Speichern von Cookies auf Ihrer Festplatte und die Anzeige von Web Beacons können Sie verhindern, indem Sie in Ihren Browser-Einstellungen ''keine Cookies akzeptieren'' wählen (Im MS Internet-Explorer unter ''Extras > Internetoptionen > Datenschutz > Einstellung''; im Firefox unter ''Extras > Einstellungen > Datenschutz > Cookies''); wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website voll umfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden."
         />
       </div>
     </section>
